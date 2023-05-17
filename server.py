4a184a8329481167519fa6f65fa8f21b8dbb15f7
Trojan13 <tim.pack@skr.reise>
2023-05-17 08:33:42 +0200

    okay i think i need to rewrite

--- server.py
+++ server.py
@@ -11,8 +11,14 @@ from EM70 import DeviceState, Command, ConnectionState, generate_packet, validat
 
 ADDRESS = "F9:8B:6F:12:EC:AE"
 CHARACTERISTICS = "64668730-033f-9393-6ca2-0e9401adeb32"
-connected_ws = asyncio.Queue()  # Update this line
-logging.basicConfig(level=logging.INFO)
+connected_ws = asyncio.Queue()  # Queue of connected websockets
+ble_client = None  # Global BLE client
+
+logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s',
+                    datefmt='%d-%m-%Y %H:%M:%S',
+                    level=logging.INFO)
+
+condition = asyncio.Condition()
 
 
 async def device_state_notify_handler(sender, data):
@@ -20,9 +26,12 @@ async def device_state_notify_handler(sender, data):
     logging.info("notify: {0}".format(list(data)))
 
     # If there is a connected websocket, send the device state to it
-    if not connected_ws.empty():
+    async with condition:
+        if connected_ws.empty():
+            # if there is no connected websocket, just return
+            return
         ws = await connected_ws.get()
-        asyncio.ensure_future(ws.send(device_state.get_json()))
+    asyncio.ensure_future(ws.send(device_state.get_json()))
 
 
 async def get_device_info(client):
@@ -32,19 +41,26 @@ async def get_device_info(client):
 
 
 async def main(ble_address: str):
+    global ble_client
     while True:
-        websocket = await connected_ws.get()
-        if not websocket.client or not websocket.client.is_connected:
+        logging.info("main")
+        async with condition:
+            if connected_ws.empty():
+                # wait for a notification if the queue is empty
+                await condition.wait()
+
+        if not ble_client or not ble_client.is_connected:
             try:
                 logging.info("Trying to connect ble...")
                 device = await BleakScanner.find_device_by_address(ble_address, timeout=20.0)
+
                 if not device:
                     raise BleakError(
                         f"A device with address {ble_address} could not be found.")
                 async with BleakClient(device) as client:
-                    websocket.client = client  # attach client to websocket object
-                    await client.start_notify(CHARACTERISTICS, device_state_notify_handler)
-                    await get_device_info(client)
+                    ble_client = client
+                    await ble_client.start_notify(CHARACTERISTICS, device_state_notify_handler)
+                    await get_device_info(ble_client)
             except BleakError as e:
                 logging.error(e)
                 # wait for 5 seconds before trying to reconnect
@@ -53,34 +69,44 @@ async def main(ble_address: str):
 
 async def websocket_handler(websocket, path):
     logging.info("WS Client connected...")
-    await connected_ws.put(websocket)
+
+    async with condition:
+        await connected_ws.put(websocket)
+        condition.notify()  # notify main that a websocket has been connected
+
     while True:
-        msg = await websocket.recv()
-        logging.info(msg)
+        try:
+            msg = await asyncio.wait_for(websocket.recv(), timeout=1.0)
+        except asyncio.TimeoutError:
+            continue
+        except websockets.exceptions.ConnectionClosed:
+            logging.info("WS Client disconnected...")
+            break
         if msg:
-            if hasattr(websocket, 'client'):
+            if ble_client and ble_client.is_connected:
+                print(validate_packet(msg))
+                print(bytes.fromhex(msg))
                 if validate_packet(msg):
-                    await websocket.client.write_gatt_char(CHARACTERISTICS, msg, True)
+                    await ble_client.write_gatt_char(CHARACTERISTICS, bytes.fromhex(msg), True)
                 else:
                     try:
                         msg_json = json.loads(msg)
                         command = msg_json.get('command', None)
                         if command == 'pause':
-                            await websocket.client.write_gatt_char(CHARACTERISTICS, generate_packet(Command.PROGRAM_PAUSE, [0x00]), True)
+                            await ble_client.write_gatt_char(CHARACTERISTICS, generate_packet(Command.PROGRAM_PAUSE, [0x00]), True)
                         elif command == 'add':
-                            await websocket.client.write_gatt_char(CHARACTERISTICS, generate_packet(Command.INTENSITY_ADD, [0x00]), True)
+                            await ble_client.write_gatt_char(CHARACTERISTICS, generate_packet(Command.INTENSITY_ADD, [0x00]), True)
                         elif command == 'cut':
                             await websocket.client.write_gatt_char(CHARACTERISTICS, generate_packet(Command.INTENSITY_CUT, [0x00]), True)
                         elif command == 'seti':
                             intensity = int(msg_json.get('value', 0))
-                            await websocket.client.write_gatt_char(CHARACTERISTICS, generate_packet(Command.INTENSITY_SET, [intensity]), True)
+                            await ble_client.write_gatt_char(CHARACTERISTICS, generate_packet(Command.INTENSITY_SET, [intensity]), True)
                     except Exception as e:
-                        await websocket.client.write_gatt_char(CHARACTERISTICS, msg, True)
+                        logging.error(e)
             else:
                 logging.info("BLE client not connected yet")
         else:
             logging.info("Received an empty message")
-        await asyncio.sleep(1)
 
 
 async def serve_websockets(loop):
